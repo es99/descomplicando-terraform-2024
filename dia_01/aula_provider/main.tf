509b190bfcfb03b3d74c5c04635a9fd922717ba8
Engels Souza <engels.franca@icloud.com>
2024-05-28 01:49:58 -0300

    dia01-aula-provider

---
+++ dia_01/aula_provider/main.tf
@@ -0,0 +1,51 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] #Canonical
+}
+
+data "aws_ami" "ubuntu-virginia" {
+  most_recent = true
+  provider    = aws.virginia
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] #Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "web-virginia" {
+  ami           = data.aws_ami.ubuntu-virginia.id
+  instance_type = "t3.micro"
+  provider      = aws.virginia
+
+  tags = {
+    Name = "HelloVirginia"
+  }
+}
---
+++ dia_01/aula_provider/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "estudosterraform-engels"
+    key    = "aula-backend"
+    region = "us-east-2"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+provider "aws" {
+  alias  = "virginia"
+  region = "us-east-1"
+}
